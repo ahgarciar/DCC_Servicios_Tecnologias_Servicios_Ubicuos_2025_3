19f68bc28a4489335e13ab6a692979ad976256a2
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-09-18 13:05:10 -0600

    up

--- P7_Knn_conModularizado/LoadInstance.py
+++ P7_Knn_conModularizado/LoadInstance.py
@@ -4,6 +4,6 @@ def load(name = "Instancia_completa.csv"):
     instancia = None
 
     if name!="":
-        instancia = pd.read_csv("../Archivos/" + name)
+        instancia = pd.read_csv("../Archivos/" + name, header=None)
 
     return instancia
